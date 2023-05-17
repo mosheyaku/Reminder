0007df2e03a76ab2884ad56134f65c52b5d9aa93
Moshe <mosheyaku@gmail.com>
2023-05-17 15:24:36 +0300

    Initialize controller

--- src/main/java/com/example/reminder/ReminderController.java
+++ src/main/java/com/example/reminder/ReminderController.java
@@ -1,7 +1,36 @@
 package com.example.reminder;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class ReminderController {
+
+    @FXML
+    private ComboBox<?> dayComboBox;
+
+    @FXML
+    private ComboBox<?> monthComboBox;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private ComboBox<?> yearComboBox;
+
+    @FXML
+    void loadPresserd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void savePressed(ActionEvent event) {
+
+    }
+
 }
